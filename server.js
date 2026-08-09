1e8573f96326121a801bbe65216719a617596682
Rajesh <rajeshelluru143@gmail.com>
2026-08-09 16:03:09 +0530

    Fix server port configuration

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
